0eedf85ca24e93f38a907a223ef6be620f5107d7
Kelechi Otugbuali <hilakechi@gmail.com>
2023-07-15 13:55:16 +0100

    SearchManufacturer component build

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+'use client'
+import React,{useState} from 'react'
+import  SearchManufacturer  from './SearchManufacturer';
+
+
+
 
 const Searchbar = () => {
+const [manufacturer,setManufacturer] = useState('')
+
+  const handleSearch=()=>{
+
+  }
   return (
-    <div>Searchbar</div>
+    <form className='searchbar' onSubmit={handleSearch}>
+          <div className='searchbar__item'>
+            <SearchManufacturer
+            manufacturer={manufacturer}
+            setManufacturer={setManufacturer}
+            />
+          </div>
+    </form>
   )
 }
 
